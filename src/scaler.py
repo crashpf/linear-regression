f268f65d8180b02371ddad537e5688b41f3312a0
crashpf <pfountas25@gmail.com>
2026-05-29 00:43:46 +0300

    added type hints, fixed sd to std in scaler.py

--- src/scaler.py
+++ src/scaler.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class StandardScaler:
     """
     From scratch implementation of StandardScaler by sklearn.
@@ -8,15 +10,15 @@ class StandardScaler:
         self.mean = None
         self.std = None
         
-    def fit(self, X):
+    def fit(self, X: np.ndarray) -> None:
         self.mean = X.mean(axis=0)
         self.std = X.std(axis=0)
     
-    def transform(self, X):
+    def transform(self, X: np.ndarray) -> np.ndarray:
         if self.mean is None:
             raise RuntimeError("Use fit() first")
-        return (X - self.mean) / self.sd
+        return (X - self.mean) / self.std
     
-    def fit_transform(self, X):
+    def fit_transform(self, X: np.ndarray) -> np.ndarray:
         self.fit(X)
         return self.transform(X)
