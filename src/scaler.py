08fc0733354586f5278204de6b384abc9bea8a38
crashpf <pfountas25@gmail.com>
2026-05-28 23:57:36 +0300

    StandardScaler implemented from scratch

--- src/scaler.py
+++ src/scaler.py
@@ -3,3 +3,15 @@ class StandardScaler:
         self.mean = None
         self.sd = None
         
+    def fit(self, X):
+        self.mean = X.mean(axis=0)
+        self.std = X.std(axis=0)
+    
+    def transform(self, X):
+        if self.mean is None:
+            raise RuntimeError("Use fit() first")
+        return (X - self.mean) / self.std
+    
+    def fit_transform(self, X):
+        self.fit(X)
+        return self.transform(X)
