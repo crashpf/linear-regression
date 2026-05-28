86049f296652fb8b7f2a5feacc395cf990eb5e53
crashpf <pfountas25@gmail.com>
2026-05-29 00:17:34 +0300

    std -> sd

--- src/scaler.py
+++ src/scaler.py
@@ -5,12 +5,12 @@ class StandardScaler:
         
     def fit(self, X):
         self.mean = X.mean(axis=0)
-        self.std = X.std(axis=0)
+        self.sd = X.sd(axis=0)
     
     def transform(self, X):
         if self.mean is None:
             raise RuntimeError("Use fit() first")
-        return (X - self.mean) / self.std
+        return (X - self.mean) / self.sd
     
     def fit_transform(self, X):
         self.fit(X)
